abcbb0a7a6f8f3630d4ef09e99242c7c26b54c3d
Lilong Zhang <and2long@gmail.com>
2025-10-04 11:56:50 +0800

    workflow

--- scripts/extract_changelog.sh
+++ scripts/extract_changelog.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-# 设置错误时退出
 set -e
 
 CHANGELOG_FILE="CHANGELOG.md"
 
-# 检查文件是否存在
 if [ ! -f "$CHANGELOG_FILE" ]; then
     echo ""
     exit 0
 fi
 
-# 提取最新版本的内容
 CHANGELOG=$(awk '
     BEGIN { found = 0; content = "" }
-    /^## \[[0-9]+\.[0-9]+\.[0-9]+\+[0-9]+\] - [0-9]{4}-[0-9]{2}-[0-9]{2}/ {
+    /^## \[[0-9]+\.[0-9]+\.[0-9]+\] - [0-9]{4}-[0-9]{2}-[0-9]{2}/ {
         if (found) exit
         found = 1
         next
@@ -27,7 +24,6 @@ CHANGELOG=$(awk '
             print ""
             exit 0
         }
-        # 移除末尾的换行符
         gsub(/\n$/, "", content)
         print content
     }
